94c4cc32b385e2d73e8315f9b715e75f519a0570
Emanuel Lindoso Perreira <emanuelstrato999@gmail.com>
2022-08-20 20:07:23 -0300

    Retornando registro ao inserir

--- GlideRecord.py
+++ GlideRecord.py
@@ -12,8 +12,8 @@ class GlideRecord:
     #Definindo os parâmetros inicias da criação da classe GlideRecord
     def __init__(self, tableName):
         self.query_data = dict()
-        self.user_name = None
-        self.password = None
+        self._user = None
+        self._password = None
         self.obj = dict()
 
         self.encodedAuth = None
@@ -28,12 +28,16 @@ class GlideRecord:
         self.__initialize = True
         self.operator = ['IN','NOT IN','STARTSWITH','ENDSWITH','CONTAINS','DOES NOT CONTAIN','INSTANCEOF']
 
+    #Implementado "Magic Method" get 
     def __getattr__(self, name):
         return self.results[self.currentIndex][name]
 
+    #Implementado "Magic Method" set 
     def __setattr__(self, name, value):
+        #verifica se todas as variáveis inicias foram criadas
         if self.__initialize and name not in self.__dict__:
-            self.obj[name] = value
+            #criando objeto para atualizar ou criar um registro
+            self.obj[name] = value 
         self.__dict__[name] = value
             
 
@@ -41,27 +45,21 @@ class GlideRecord:
     def set_server(self, servername):
         self.query_data['server'] = servername
 
-    def set_credentials(self, user_name, password):
-        self.user_name = user_name
-        self.password = password
-        self.reset_credentials()
+    #método para definir as credenciais
+    def set_credentials(self, user, password):
+        self._user = user
+        self._password = password
 
     #lê a entrada das credenciais do usuário no Servicenow
     def get_credentials(self):
-        self.user_name = input("Digite seu usuário Servicenow: ")
-        self.password = getpass.getpass("Digite a sua senha: ")
-        self.reset_credentials()
+        self._user = input("Digite seu usuário Servicenow: ")
+        self._password = getpass.getpass("Digite a sua senha: ")
 
-
-    def reset_credentials(self):
-        string = self.user_name + ":" + self.password
-        self.encodedAuth = base64.b64encode(
-            string.encode()
-        )
-
+    #retorna a quantidade registros encontrados ao realizar a query
     def getRowCount(self):
         return len(self.results)
 
+    #retorna um registro específico a partir do sys_id ou do par key e value
     def get(self, *args):
         if(len(args) == 1):
             key = args[0]
@@ -84,11 +82,13 @@ class GlideRecord:
                 return True
             return False
 
+    #verifica se ainda existe algum registro na lista
     def hasNext(self):
         if self.getRowCount() > 0 and self.currentIndex + 1 < self.getRowCount():
             return True
         return False
 
+    #pega o próximo registro da query
     def next(self):
         self.obj = {}
         if self.query_data['rowCount'] > 0 and self.currentIndex + 1 < self.query_data['rowCount']:
@@ -96,6 +96,7 @@ class GlideRecord:
             return True
         return False
 
+    #adiciona uma query
     def addQuery(self, *args):
         if(len(args) == 2):
             key = args[0]
@@ -112,45 +113,48 @@ class GlideRecord:
             if self.query_data['sysparm_query'] == "":
                 self.query_data['sysparm_query'] +=  key + operator + value
             else:
-                self.query_data['sysparm_query'] += "^" + key + operator + valu
+                self.query_data['sysparm_query'] += "^" + key + operator + value
 
+    #adiciona uma encoded query
     def addEncodedQuery(self, queryString):
         if self.query_data['sysparm_query'] == "":
             self.query_data['sysparm_query'] = queryString
         else:
             self.query_data['sysparm_query'] += "^" + queryString
 
+    #ordena os resultado a partir de um campo escolhido em ordem crescente
     def orderBy(self, field):
         if self.query_data['sysparm_query'] == "":
             self.query_data['sysparm_query'] = "ORDERBY" + field
         else:
             self.query_data['sysparm_query'] = "^ORDERBY" + field
 
+    #ordena os resultado a partir de um campo escolhido em ordem decrescente
     def orderByDesc(self, field):
         if self.query_data['sysparm_query'] == "":
             self.query_data['sysparm_query'] = "ORDERBYDESC" + field
         else:
             self.query_data['sysparm_query'] = "^ORDERBYDESC" + field
 
+    #manda a query montado para o endpoit da instância através do método GET
     def query(self):
         self.currentIndex = -1
         req = requests.get(
             self.query_data['server']+"/api/now/table/"+self.query_data['tableName']+"?sysparm_query="+
             self.query_data['sysparm_query'] + 
             "^sysparm_display_value=false&sysparm_exclude_reference_link=true&sysparm_limit="+self.query_data['limit'], 
-            auth=(self.user_name, self.password)
+            auth=(self._user, self._password)
         )
         self.results = json.loads(req.content)
         self.results = self.results['result']
         self.query_data['rowCount'] = len(self.results)
 
+    #retorna o value de uma campo específico
     def getValue(self, key):
         rs = self.results[self.currentIndex][key]
         return rs    
 
-    def getValue(self, key, value=""):
-        self.results[self.currentIndex][key] = value
-
+    #atualizamos o valor de um campo e a lista de objetos a ser mandando para atualizar o registro
     def setValue(self, field, value=""):
         self.obj[field] = value
         self.results[self.currentIndex] = value
@@ -158,24 +162,33 @@ class GlideRecord:
     def setLimit(self, limit):
         self.query_data['limit'] = str(limit)
 
-
+    #atualizar um registro
     def update(self):
         sys_id = self.results[self.currentIndex]['sys_id']
         data = json.dumps(self.obj)
-        req = requests.put(self.query_data['server']+"/api/now/table/"+self.query_data['tableName']+"/"+sys_id, auth=(self.user_name, self.password), data=data)
+        req = requests.put(self.query_data['server']+"/api/now/table/"+self.query_data['tableName']+"/"+sys_id, auth=(self._user, self._password), data=data)
         print(req.status_code)
         print(req.content)
 
+    #resetando dicionário para inserir um novo registro na tabela
     def initialize(self):
         self.obj = {}
         
+    #inserindo um novo registro na tabela
     def insert(self):
         data = json.dumps(self.obj)
-        
-        req = requests.post(self.query_data['server']+"/api/now/table/"+self.query_data['tableName'], auth=(self.user_name, self.password), data=data)
+        req = requests.post(self.query_data['server']+"/api/now/table/"+self.query_data['tableName'] + "?sysparm_display_value=false&sysparm_exclude_reference_link=true", 
+        auth=(self._user, self._password), data=data)
         print(req.status_code)
         print(req.content)
 
+        results = json.loads(req.content)
+        self.results = []
+        self.results.append(results['result'])
+        self.currentIndex = 0
+        self.query_data['rowCount'] = 0
+
+    #pesquisa se um determinado registro existe a partir de um campo e seu valor
     def find(self, field, value):
         for index, record in enumerate(self.results):
             if record[field] == value:
@@ -188,7 +201,7 @@ class GlideRecord:
         self.delete(sys_id)
 
     def delete(self, sys_id):
-        req = requests.delete(self.query_data['server']+"/api/now/table/"+self.query_data['tableName']+"/"+sys_id, auth=(self.user_name, self.password))
+        req = requests.delete(self.query_data['server']+"/api/now/table/"+self.query_data['tableName']+"/"+sys_id, auth=(self._user, self._password))
         print(req.status_code)
         print(req.content)
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from GlideRecord import *
 from Credentials import *
 
-gr = GlideRecord("incident")
+gr = GlideRecord("sys_user")
 
 gr.set_server("https://dev81975.service-now.com")
 gr.set_credentials(user, password)
 
-gr.get('sys_id','1c832706732023002728660c4cf6a7b9')
-print(gr.number)
+gr.addQuery('name', 'STARTSWITH', 'Emanuel')
+gr.query()
+
+gr.deleteMultiple()
+
 
 # gr.caller_id = "6816f79cc0a8016401c5a33be04be441"
 # gr.short_description = "Criando um incident a partir do Python"
