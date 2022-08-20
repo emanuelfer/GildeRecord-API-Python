3f2fbcda117b42396cb97b337a66ebb84f8b3829
Emanuel Lindoso Perreira <emanuelstrato999@gmail.com>
2022-08-20 20:55:08 -0300

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from GlideRecord import *
 from Credentials import *
 
-gr = GlideRecord("sys_user")
 
-gr.set_server("https://dev81975.service-now.com")
+from GlideRecord import *
+
+gr = GlideRecord("incident")
+
+gr.set_server("https://instace.service-now.com")
 gr.set_credentials(user, password)
 
-gr.addQuery('name', 'STARTSWITH', 'Emanuel')
-gr.query()
+gr.initialize()
 
-gr.deleteMultiple()
+gr.short_description = "Computador reiniciando sozinho!"
+gr.category = "hardware"
+gr.impact = 1
+gr.urgency = 1
+
+gr.insert()
 
 
-# gr.caller_id = "6816f79cc0a8016401c5a33be04be441"
-# gr.short_description = "Criando um incident a partir do Python"
-# gr.description = "ServiceNow GlideRecord API allows you to Create a record using Python"
-# gr.assignment_group = "ServiceNow GlideRecord API allows you to Create a record using Python"
-# gr.insert()
 
